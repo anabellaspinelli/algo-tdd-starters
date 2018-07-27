31a85b344ccb1c4b4937c9d25d2690d87cfb5644
Anabella Spinelli <anabellaspinelli@gmail.com>
2018-07-27 14:50:37 +0200

    Busy Days v1

--- node-es6/src/__test__/task1.test.js
+++ node-es6/src/__test__/task1.test.js
@@ -2,10 +2,21 @@ var task1 = require('../task1');
 
 describe('task 1 solver', () => {
   it('should return 7', () => {
-  	expect(task1.solve()).toEqual(7);
+    expect(task1.solve([['2018-01-01', '2018-01-01']])).toEqual('2018-01-01');
   });
 
-  it('should be able to map an array', () => {
-    expect(task1.solveWithEs6Feature(2)).toEqual([4, 10, 14]);
+  it('should return 7', () => {
+    expect(task1.solve([
+      ['2018-01-01', '2018-01-03'],
+      ['2018-01-02', '2018-01-03']
+    ])).toEqual('2018-01-02');
+  });
+
+  it('should return 7', () => {
+    expect(task1.solve([
+      ['2018-01-01', '2019-01-01'],
+      ['2018-06-01', '2019-06-03'],
+      ['2020-01-02', '2021-01-03']
+    ])).toEqual('2018-06-01');
   });
 })
--- node-es6/src/task1.js
+++ node-es6/src/task1.js
@@ -1,10 +1,49 @@
+// Complete the solve function below.
+function solve(reservations) {
+  let reservationsPerDay = {}
+  const SECONDS_IN_A_DAY = 86400;
 
-function solve() {
-  return 7
+  reservations.forEach(reservation => {
+
+    // Parse reservation entry
+    let start = Date.parse(reservation[0]);
+    let end = Date.parse(reservation[1]);
+
+    // Process every day in a reservation
+    let currentDate = start;
+
+    while (currentDate <= end) {
+      if (!reservationsPerDay[`${currentDate}`]) {
+        reservationsPerDay[`${currentDate}`] = 1
+        // } else {
+      }
+
+      reservationsPerDay[`${currentDate}`] += 1;
+      currentDate += SECONDS_IN_A_DAY;
+    }
+  })
+
+  // Find the day with the most reservations
+  let maxReservations = 0;
+  let busiestDay = Number.MAX_SAFE_INTEGER;
+
+  for (let day in reservationsPerDay) {
+    if (reservationsPerDay[day] > maxReservations) {
+      busiestDay = day
+      maxReservations = reservationsPerDay[day];
+    } else if (reservationsPerDay[day] === maxReservations) {
+      if (day < busiestDay) {
+        busiestDay = day
+      }
+    }
+  }
+
+  return new Date(parseInt(busiestDay, 10)).toISOString().split('T')[0]
+
 }
 
 function solveWithEs6Feature(a) {
-  return [a, 5, 7].map(n => n*2)
+  return [a, 5, 7].map(n => n * 2)
 }
 
 module.exports.solve = solve;
